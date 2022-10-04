4fe39f2c8e9cb4fcb011386b898be6ab573e1d23
TEMFACK DERICK <dericktemfack@gmail.com>
2022-10-04 12:23:17 +0100

    save

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 Route::post('ckeditor/upload', [CKEditorController::class, 'upload'])->name('ckeditor.image-upload');
 
-Route::get("/admin/new-post", [PostController::class, 'index']);
+Route::get("/admin/new-post", [PostController::class, 'index'])->name('new-post');
 Route::post('/admin/create-post', [PostController::class, 'createPost'])->name('create-post');
 Route::get('/admin/update-post/{slug}/', [PostController::class, 'editPostForm']);
 Route::post('/admin/save-post/{slug}/', [PostController::class, 'save'])->name('name');
